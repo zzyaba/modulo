4003388fb6747bf5a2b7d72294b6939d0c829780
zzyaba <zhuziyuenky@163.com>
2025-06-05 18:00:18 +0800

    Update modulo.hpp

--- modulo.hpp
+++ modulo.hpp
@@ -1,61 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define T T
-#define N 100001
-T T,n,m,t,x,y,ans,M,a[N],p[N],b[N],o[N];
-priority_queue<T,vector<T>,greater<T>>q;
-void exgcd(T a,T b,T&x,T&y){
-	if(b==0){
-		x=1;
-		y=0;
-	}else{
-		exgcd(b,a%b,y,x);
-		y-=a/b*x;
+template<typename T=long long,T mmod=998244353>
+struct modulo{
+	constexpr static T mod=mmod;
+	vector<T>fac={1,1},inv={1,1},ifc={1,1};
+	inline T factorial(const T&x){
+		while(fac.size()<=x){
+			fac.push_back(fac.back()*fac.size()%mod);
+		}
+		return fac[x];
 	}
-}
+	inline T inverse(const T&x){
+		while(inv.size()<=x){
+			inv.push_back(inv[mod%inv.size()]*(mod-mod/inv.size())%mod);
+		}
+		return inv[x];
+	}
+	inline T invfact(const T&x){
+		while(inv.size()<=x){
+			inv.push_back(inv[mod%inv.size()]*(mod-mod/inv.size())%mod);
+		}
+		while(ifc.size()<=x){
+			ifc.push_back(ifc.back()*inv[ifc.size()]%mod);
+		}
+		return ifc[x];
+	}
+	inline T arrange(const T&x,const T&y){
+		return factorial(x)*invfact(y-x)%mod;
+	}
+	inline T combine(const T&x,const T&y){
+		return factorial(x)*invfact(y-x)%mod*invfact(y)%mod;
+	}
+	inline T sum(T x,const T&y){
+		return(x+=y)>=mod?x-mod:x;
+	}
+	inline T sub(T x,const T&y){
+		return(x-=y)<0?x+mod:x;
+	}
+	inline T mul(const T&x,const T&y){
+		return x*y%mod;
+	}
+	inline T div(const T&x,const T&y){
+		return x*inverse(y)%mod;
+	}
+	inline T norm(T x){
+		return(x%=mod)<0?x+mod:x;
+	}
+};
 signed main(){
-	#ifdef ONLINE_JUDGE
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0); 
-	#else
-	freopen("1.in","r",stdin);
-	#endif
-	cin>>T;
-	while(T--){
-		cin>>n>>m;
-		for(T i=1;i<=n;i++){
-			cin>>a[i];
-		}
-		for(T i=1;i<=n;i++){
-			cin>>p[i];
-		}
-		for(T i=1;i<=n;i++){
-			cin>>o[i];
-		}
-		while(m--){
-			cin>>x;
-			q.push(x);
-		}
-		for(T i=1;i<=n;i++){
-			b[i]=q.top();
-			q.pop();
-			q.push(o[i]);
-		}
-		ans=0;
-		M=1;
-		for(T i=1;i<=n;i++){
-			exgcd(md(b[i]*M),p[i],x,y);
-			x=md(x);
-			if((a[i]-b[i]*ans)%__gcd(x,y)){
-				ans=-1;
-				break;
-			}
-			p[i]/=__gcd(x,y);
-			ans+=(a[i]-b[i]*ans)/__gcd(x,y)*x%p[i]*M;
-			M*=p[i];
-			ans%=M;
-		}
-		cout<<ans<<endl;
-	}
+	modulo<>md;
 }
